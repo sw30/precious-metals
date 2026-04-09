88153f2e096d058f7701365afbcf88b7bb2b8c5b
Stanislaw Wolny <stanislaw.wolny30@gmail.com>
2026-04-09 09:52:01 +0200

    [PM-10] - Frontend NPM initial commit, with basic dependencies: LIT, LIT CONTEXT, VAADIN ROUTER, VITE(dev-server)

---
+++ client/src/main.js
@@ -0,0 +1,11 @@
+import './style.css';
+import { Router } from '@vaadin/router';
+
+const outlet = document.getElementById('outlet');
+const router = new Router(outlet);
+
+router.setRoutes([
+
+]);
+
+console.log('Router initialized');
